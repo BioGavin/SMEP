b093b6bcfcc476f7b1aad40342934c1e2f72b8c9
Zhen-Yi Zhou <gavinchou99@126.com>
2024-02-07 17:04:24 +0800

    Update gen_featured_data_for_cAMP.py

--- gen_featured_data_for_cAMP.py
+++ gen_featured_data_for_cAMP.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     input_tsv, filtered_data_tsv, featured_data_csv = sys.argv[1: 4]
     # input_tsv = "task/GEM_GMBC_ripp_camp.result.tsv"
     df = pd.read_csv(input_tsv, sep='\t')
+    df = df[~df["sequence"].str.contains("B|J|X|Z|O|U")] 
     df = df[df["seq"].apply(lambda x: 5 < len(x) < 50)].reset_index(drop=True)
     df.to_csv(filtered_data_tsv, sep='\t', index=False)
     sequence = df["seq"]
